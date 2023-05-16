6664e9aaa985dbca584b2091478a55d05451846f
Yamayamaaya <yugo139013@gmail.com>
2023-05-16 23:09:35 +0900

    create Utils style sheet

--- pages/compornents/Layout.js
+++ pages/compornents/Layout.js
@@ -1,15 +1,23 @@
 import Head from "next/head";
+import styles from "./Layout.module.css";
+import utilsStyles from "@/styles/utils.module.css";
 
 function Layout({ children }) {
   return (
-    <div>
+    <div className={styles.container}>
       <Head>
         <title>Yamayamaaya Blog</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <header>
-        <img src="/images/profile.jpg" alt="Your Name" />
-        <h1>Yamayamaaya</h1>
+      <header className={styles.header}>
+        <img
+          className={utilsStyles.borderCircle}
+          src="/images/profile.jpg"
+          alt="Your Name"
+          width={144}
+          height={144}
+        />
+        <h1 className={utilsStyles.heading2Xl}>Yamayamaaya</h1>
       </header>
       <main>{children}</main>
     </div>
