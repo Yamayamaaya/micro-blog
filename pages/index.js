3d9962d1ea2d0336509053be83ed46594e33dc80
Yamayamaaya <yugo139013@gmail.com>
2023-05-17 21:35:54 +0900

    format Layout

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,82 @@
 import Head from "next/head";
 import Image from "next/image";
+import Link from "next/link";
 import { Inter } from "next/font/google";
 import styles from "@/styles/Home.module.css";
 import Layout from "./compornents/Layout";
+import utilsStyle from "@/styles/utils.module.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
     <>
-      <Layout></Layout>
+      <Layout>
+        <section className={utilsStyle.headingMd}>
+          <p>こんにちは！</p>
+        </section>
+        <section className={`${utilsStyle.headingMd} ${utilsStyle.padding1px}`}>
+          <h2>最近の投稿</h2>
+          <div className={styles.grid}>
+            <article>
+              <Link href="/posts/firstPost">
+                <img
+                  src="/images/thumbnail01.jpg"
+                  className={styles.thumbnailImage}
+                  alt=""
+                />
+              </Link>
+              <Link href="/posts/firstPost">
+                <p className={utilsStyle.boldText}>これはテストの投稿</p>
+              </Link>
+              <br />
+              <small className={utilsStyle.lightText}>2021-10-10</small>
+            </article>
+            <article>
+              <Link href="/posts/firstPost">
+                <img
+                  src="/images/thumbnail01.jpg"
+                  className={styles.thumbnailImage}
+                  alt=""
+                />
+              </Link>
+              <Link href="/posts/firstPost">
+                <p className={utilsStyle.boldText}>これはテストの投稿</p>
+              </Link>
+              <br />
+              <small className={utilsStyle.lightText}>2021-10-10</small>
+            </article>
+            <article>
+              <Link href="/posts/firstPost">
+                <img
+                  src="/images/thumbnail01.jpg"
+                  className={styles.thumbnailImage}
+                  alt=""
+                />
+              </Link>
+              <Link href="/posts/firstPost">
+                <p className={utilsStyle.boldText}>これはテストの投稿</p>
+              </Link>
+              <br />
+              <small className={utilsStyle.lightText}>2021-10-10</small>
+            </article>
+            <article>
+              <Link href="/posts/firstPost">
+                <img
+                  src="/images/thumbnail01.jpg"
+                  className={styles.thumbnailImage}
+                  alt=""
+                />
+              </Link>
+              <Link href="/posts/firstPost">
+                <p className={utilsStyle.boldText}>これはテストの投稿</p>
+              </Link>
+              <br />
+              <small className={utilsStyle.lightText}>2021-10-10</small>
+            </article>
+          </div>
+        </section>
+      </Layout>
     </>
   );
 }
